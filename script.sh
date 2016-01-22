2f14e93f57b11b64003781cc8510eb55ee11e7ef
rafapicazo <rafa_8_6_96@hotmail.com>
2016-01-22 10:17:38 +0000

    borrados

--- script.sh
+++
@@ -1,6 +0,0 @@
-git config --global http.sslVerify false
-git clone https://github.com/rafajare/PruebasMaceda.git
-#cp -R /entrada/PruebasMaceda/app/* /web
-#rm -rf/entrada/PruebasMaceda/*
-date > log.txt
-
